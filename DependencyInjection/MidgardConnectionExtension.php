bf5126c4b95379d432fd3c11d79397abe4d626b1
Henri Bergius <henri.bergius@iki.fi>
2011-07-27 19:05:41 +0300

    Move opening of DB connection to bundle boot

--- DependencyInjection/MidgardConnectionExtension.php
+++ DependencyInjection/MidgardConnectionExtension.php
@@ -17,21 +17,6 @@ class MidgardConnectionExtension extends Extension
         $loader->load('connection.xml');
 
         $setup = $configs[0];
-
-        $config = new \midgard_config();
-        $config->dbtype = $setup['type'];
-        $config->database = $setup['name'];
-        $config->dbdir = $setup['databasedir'];
-        $config->logfilename = $setup['logfile'];
-        $config->loglevel = $setup['loglevel'];
-        $config->blobdir = $setup['blobdir'];
-        $config->sharedir = $setup['sharedir'];
-
-        $connection = \midgard_connection::get_instance();
-        if (!$connection->open_config($config)) {
-            throw new \RuntimeException('Failed to open Midgard2 connection: ' . $connection->get_error_string());
-        }
-
         $container->setParameter('midgard.connection.config', $setup);
     }
 }
--- MidgardConnectionBundle.php
+++ MidgardConnectionBundle.php
@@ -5,4 +5,22 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MidgardConnectionBundle extends Bundle
 {
+    public function boot()
+    {
+        $setup = $this->container->getParameter('midgard.connection.config');
+
+        $config = new \midgard_config();
+        $config->dbtype = $setup['type'];
+        $config->database = $setup['name'];
+        $config->dbdir = $setup['databasedir'];
+        $config->logfilename = $setup['logfile'];
+        $config->loglevel = $setup['loglevel'];
+        $config->blobdir = $setup['blobdir'];
+        $config->sharedir = $setup['sharedir'];
+
+        $connection = \midgard_connection::get_instance();
+        if (!$connection->open_config($config)) {
+            throw new \RuntimeException('Failed to open Midgard2 connection: ' . $connection->get_error_string());
+        }
+    }
 }
